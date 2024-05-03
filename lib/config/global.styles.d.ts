85340503a74a63870ba5e154235022e4ae47507d
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-05-03 15:56:09 +0100

    update: development

--- lib/config/global.styles.d.ts
+++ lib/config/global.styles.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react" />
-import './fonts/styles.css';
+import "./fonts/styles.css";
 export declare const GlobalStyles: import("react").NamedExoticComponent<import("styled-components").ExecutionProps & object>;
 //# sourceMappingURL=global.styles.d.ts.map
